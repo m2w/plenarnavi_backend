46eaf8b29cf25b78550d6547061fb2b98cb68f17
Johannes <johannes@wji.me>
2017-10-13 20:40:43 +0200

    add routes for person resource

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ import uuid
 
 from api.resources import SpeechResource, SpeechListResource
 from api.resources import SessionResource, SessionListResource
+from api.resources import PersonResource, PersonListResource
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,8 @@ api.add_resource(SpeechResource, '/speeches/<uuid:uuid>', endpoint='speech')
 api.add_resource(SpeechListResource, '/speeches', endpoint='speeches')
 api.add_resource(SessionResource, '/sessions/<uuid:uuid>', endpoint='session')
 api.add_resource(SessionListResource, '/sessions', endpoint='sessions')
+api.add_resource(PersonResource, '/persons/<uuid:uuid>', endpoint='person')
+api.add_resource(PersonListResource, '/persons', endpoint='persons')
 
 @app.teardown_appcontext
 def close_db(error):
