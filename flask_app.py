79cdbd72e388cdd62a40a83c1efe89caa0ccb055
Moritz <oss@tibidat.com>
2017-10-15 18:29:40 +0200

    Add a CORS quickfix and html5lib dependency

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,16 @@ from api.resources import PersonResource, PersonListResource
 app = Flask(__name__)
 api = Api(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add(
+        'Access-Control-Allow-Headers',
+        'Content-Type,Authorization'
+    )
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST')
+    return response
+
 api.add_resource(SpeechResource, '/speeches/<uuid:uuid>', endpoint='speech')
 api.add_resource(SpeechListResource, '/speeches', endpoint='speeches')
 api.add_resource(SessionResource, '/sessions/<uuid:uuid>', endpoint='session')
